47466aea88943f59723c74a479e48ece80d6d538
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-21 18:20:26 +0530

    excercise 3 complete

--- VideoRent/Controllers/MoviesController.cs
+++ VideoRent/Controllers/MoviesController.cs
@@ -22,6 +22,14 @@ namespace VideoRent.Controllers
             var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
         // GET: Movies/Random   
         public ActionResult Random()
         {
