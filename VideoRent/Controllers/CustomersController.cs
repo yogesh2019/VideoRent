b53f3aa2adb8bef6f1826421611075df6cdefd4f
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-22 10:37:05 +0530

    updating data and entity errors

--- VideoRent/Controllers/CustomersController.cs
+++ VideoRent/Controllers/CustomersController.cs
@@ -44,9 +44,21 @@ namespace VideoRent.Controllers
         } 
         
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if(customer.Id == 0)
+            {
+               _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewLetter = customer.IsSubscribedToNewLetter;
+            }
+           
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");
         }
