fe6e487f6c01c678bd7f71134b97261f71f74cc5
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-22 11:56:59 +0530

    adding Validation

--- VideoRent/Controllers/CustomersController.cs
+++ VideoRent/Controllers/CustomersController.cs
@@ -46,6 +46,15 @@ namespace VideoRent.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if(customer.Id == 0)
             {
                _context.Customers.Add(customer);
